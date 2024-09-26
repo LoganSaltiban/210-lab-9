553b58dc7b10c668b84b8bed732dbc9347379603
Logan Saltiban <logansaltiban@Logans-MacBook-Air.local>
2024-09-26 16:47:12 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for ()
+    for (int i = 1; i < 101; i++)
+    {
         cout << i << " ";
+    }
 
     return 0;
 }
